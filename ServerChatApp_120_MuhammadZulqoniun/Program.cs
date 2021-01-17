ca3a41bc6e18ed0f3a9bca8f2cb11c863e704c70
Zulqoniun <muhammadqoni123@gmail.com>
2021-01-17 18:40:19 +0700

    Add project files.

---
+++ ServerChatApp_120_MuhammadZulqoniun/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_120;
+using System.ServiceModel;
+
+namespace ServerChatApp_120_MuhammadZulqoniun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObject = null;
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
